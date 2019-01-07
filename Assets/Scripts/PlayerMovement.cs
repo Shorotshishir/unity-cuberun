92965a6674639e133fdb44588a4738b310e37314
Shorotshishir <tanzim121@gmail.com>
2019-01-07 10:57:00 +0600

    for android
    Signed-off-by: Shorotshishir <tanzim121@gmail.com>

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour {
+
     public Rigidbody rb;
     //public float forwarfForce = 1000f;
     public float sideForce = 500f;
     public float speed = 15f;
     public float mapWidth = 2f;
+
+    public Joystick joystick;
     // Update is called once per frame
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
     void FixedUpdate () {
-        rb = GetComponent<Rigidbody>();
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        //rb = GetComponent<Rigidbody>();
+        //float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float x = joystick.Horizontal * Time.deltaTime * speed;
         Vector3 newPosition = rb.position + Vector3.right * x;
         rb.MovePosition(newPosition);
         if (rb.position.y < -1f)
